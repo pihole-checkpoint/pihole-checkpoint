4688c10d183ccc70ddcd6cb7e6f77cb52c65837a
Leonardo Merza <ljmerza@gmail.com>
2026-01-18 14:34:01 -0500

    Change default ALLOWED_HOSTS to wildcard
    More permissive default for Docker deployments where the
    hostname may not be known in advance.

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-in-producti
 
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
